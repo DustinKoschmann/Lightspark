469df61e867f73c316ff61837234d4e3ea08fb53
Dustin Koschmann <Dustin.Koschmann@web.de>
2018-06-23 02:51:29 +0200

    First Commit

---
+++ Lightspark/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float movementSpeed = 100.0f;
+    public float mouseSensitivity = 2f;
+    public float maxVel = 20f;
+
+
+    private CharacterController controller;
+    private bool isMoving = false;
+
+    float xVal = 0f;
+    float yVal = 0f;
+
+    // Use this for initialization
+    void Start () {
+        controller = this.transform.GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        wasdMovement();
+        mouseMovement();
+	}
+
+    void wasdMovement() {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(horizontalInput, 0f, verticalInput);
+
+        Vector3 forward = this.transform.TransformDirection(move);
+
+
+        if(verticalInput != 0 || horizontalInput != 0) {
+            controller.SimpleMove(forward * movementSpeed);
+            isMoving = true;
+        } else {
+            isMoving = false;
+        }
+
+        if(!controller.isGrounded) {
+            controller.Move(new Vector3(0f, -1f, 0f));
+        }
+    }
+
+    void mouseMovement() {
+        if(xVal >= 360f) {
+            xVal = 0f;
+        }
+        if(yVal >= 360f) {
+            yVal = 0f;
+        }
+
+
+        xVal += mouseSensitivity * Input.GetAxis("Mouse X");
+        yVal -= mouseSensitivity * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(yVal, xVal, 0.0f);
+
+        if(Input.GetButtonDown("Fire1")) {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public bool checkMoving() {
+        return isMoving;
+    }
+}
---
+++ Lightspark/Assets/Scripts/Torchscript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torchscript : MonoBehaviour {	
+    public GameObject parentObject;
+    public float shakeIntensity = 2f;
+
+    private float maxShake = .1f;
+    private float curShake = 0f;
+    private bool shakeUp = false;
+    private bool allowShake = false;
+
+    private PlayerMovement playerMovement;
+
+    Vector3 startPos;
+    Quaternion lockedRotation;
+
+
+    void Start() {
+        startPos = this.transform.position;
+        lockedRotation = this.transform.rotation;
+        playerMovement = parentObject.transform.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(playerMovement.checkMoving()) {
+            allowShake = true;
+        } else {
+            allowShake = false;
+        }
+
+        if(allowShake) {
+            if(shakeUp && curShake >= maxShake) {
+                shakeUp = false;
+            }
+            if(!shakeUp && curShake <= -maxShake) {
+                shakeUp = true;
+            }
+            if(shakeUp) {
+                curShake += Time.deltaTime * shakeIntensity;
+            } else {
+                curShake -= Time.deltaTime * shakeIntensity;
+            }
+        } else {
+            curShake = 0f;
+        }
+        
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + curShake, this.transform.position.z);
+
+        lockedRotation = Quaternion.Euler(lockedRotation.eulerAngles.x, this.transform.rotation.eulerAngles.y, this.transform.rotation.eulerAngles.z);
+    }
+
+    private void LateUpdate() {
+        this.transform.rotation = lockedRotation;
+    }
+}
